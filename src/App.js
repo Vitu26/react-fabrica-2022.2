ce5b1377119ea163e771830c4139f830538dde80
Víctor Barreto <victorbarretolins@gmail.com>
2022-09-14 15:35:31 -0300

    usando map para renderizar os mosntros no app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import dados from "./dados/dadosFake";
 
 function App() {
+  const [dadosFake, setDadosFake] = useState(dados);
+
   return (
     <main>
       <section className="container">
         <h1>monstros</h1>
 
-        <article className="monstros">
-          <img src="https://robohash.org/1?set=set2" alt="monstro" />
-          <div>
-            <h2>nome</h2>
-            <p>email</p>
-          </div>
-        </article>
+        {dadosFake.map((objeto) => {
+          return (
+            <article className="monstros">
+              <img src="https://robohash.org/1?set=set2" alt="monstro" />
+              <div>
+                <h2>nome</h2>
+                <p>email</p>
+              </div>
+            </article>
+          );
+        })}
 
         <button className="btn-azul" type="button">
           limpar monstros

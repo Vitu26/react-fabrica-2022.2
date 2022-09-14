e8351ad41b36a3864e3706eb4e4cf838680edb07
Víctor Barreto <victorbarretolins@gmail.com>
2022-09-14 17:32:27 -0300

    finalizado componente para monstros

--- src/components/ListaMonstros.jsx
+++ src/components/ListaMonstros.jsx
@@ -1,25 +1,14 @@
-import { Fragment } from "react";
+import Monstro from "./Monstro";
 
 const ListaMonstros = (props) => {
   const dados = props.dados;
 
   return (
-    <Fragment>
+    <>
       {dados.map((objeto) => {
-        return (
-          <article key={objeto.id} className="monstros">
-            <img
-              src={`https://robohash.org/${objeto.id}?set=set2`}
-              alt={objeto.name}
-            />
-            <div>
-              <h2>{objeto.name}</h2>
-              <p>{objeto.email}</p>
-            </div>
-          </article>
-        );
+        return <Monstro monstroUnico={objeto} />;
       })}
-    </Fragment>
+    </>
   );
 };
 
